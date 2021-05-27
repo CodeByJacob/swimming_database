a416310cb326fd688b102e13699829a00d817847
CodeByJacob <kubawojtas34@gmail.com>
2021-05-27 23:06:40 +0200

    Added deletes/values_creating_query.sql and modified tables

---
+++ deletes_creating_query.sql
@@ -0,0 +1,7 @@
+DROP TABLE zawodnik_zawody;
+DROP TABLE konkurencja;
+DROP TABLE trener;
+DROP TABLE zawodnik;
+DROP TABLE zawody;
+DROP TABLE klubplywacki;
+DROP TABLE licencja;
--- tables_creating_query.sql
+++ tables_creating_query.sql
@@ -15,7 +15,7 @@
 -- -------------------------------------------------------------
 -- -------------------------------------------------------------
 --
---               SKRYPT DODAJACY TABELE DO BAZY DANYCH
+--            SKRYPT DODAJACY TABELE DO BAZY DANYCH
 --
 -- -------------------------------------------------------------
 
@@ -24,15 +24,24 @@
 -- -------------------------------------------------------------
 
 -- -------------------------------------------------------------
--- STRUKTURA TABELI REPREZENTUJACEJ KLUB_PLYWACKI
+-- STRUKTURA TABELI REPREZENTUJACEJ KLUBPLYWACKI
 
-CREATE TABLE IF NOT EXISTS pbd_plywanie.KlubPlywacki (
+CREATE TABLE IF NOT EXISTS KlubPlywacki (
   ID_Klubu INTEGER(5) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
   Nazwa VARCHAR(50) NOT NULL,
   Adres VARCHAR(50) NOT NULL,
   IloscCzlonkow INTEGER DEFAULT NULL,
-  DataPowstania DATE NOT NULL,
-  Kapital DECIMAL(10,2) NOT NULL
+  DataPowstania DATE NOT NULL
+) ENGINE = InnoDB;
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ LICENCJA
+
+CREATE TABLE IF NOT EXISTS Licencja (
+  ID_Licencji INTEGER(5) AUTO_INCREMENT PRIMARY KEY  NOT NULL,
+  DataWaznosciLicencji DATE NOT NULL
 ) ENGINE = InnoDB;
 
 -- -------------------------------------------------------------
@@ -45,7 +54,43 @@ CREATE TABLE IF NOT EXISTS pbd_plywanie.KlubPlywacki (
 -- -------------------------------------------------------------
 
 -- -------------------------------------------------------------
--- STRUKTURA TABELI REPREZENTUJACEJ Sedzia
+-- STRUKTURA TABELI REPREZENTUJACEJ SEDZIA
+
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ TRENER
+
+CREATE TABLE IF NOT EXISTS Trener (
+  ID_Trenera INTEGER(5) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+  Imie VARCHAR(30)  NOT NULL,
+  Nazwisko VARCHAR(40) NOT NULL,
+  DataUrodzenia DATE NOT NULL,
+  Plec ENUM(
+      'M',  -- MEZCZYZNA
+      'K'   -- KOBIETA
+      ) NOT NULL,
+  ID_Licencji INTEGER(5) NOT NULL,
+  ID_Klubu INTEGER(5) DEFAULT NULL
+) ENGINE = InnoDB;
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ WYNIK
+
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ ETAP
+
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ REKORDOSOBISTY
 
 
 -- -------------------------------------------------------------
@@ -53,7 +98,7 @@ CREATE TABLE IF NOT EXISTS pbd_plywanie.KlubPlywacki (
 -- -------------------------------------------------------------
 -- STRUKTURA TABELI REPREZENTUJACEJ ZAWODY
 
-CREATE TABLE IF NOT EXISTS pbd_plywanie.Zawody (
+CREATE TABLE IF NOT EXISTS Zawody (
     ID_Zawodow INTEGER(5) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
     Miasto VARCHAR(50) NOT NULL,
     Kraj VARCHAR(50) NOT NULL,
@@ -69,30 +114,90 @@ CREATE TABLE IF NOT EXISTS pbd_plywanie.Zawody (
 -- -------------------------------------------------------------
 
 -- -------------------------------------------------------------
--- STRUKTURA TABELI REPREZENTUJACEJ TRENERA
+-- STRUKTURA TABELI REPREZENTUJACEJ ZAWODNIK
 
-CREATE TABLE IF NOT EXISTS pbd_plywanie.Trener (
-  ID_Trenera INTEGER(5) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
-  Imie VARCHAR(30)  NOT NULL,
-  Nazwisko VARCHAR(40) NOT NULL,
-  DataUrodzenia DATE NOT NULL,
-  Plec ENUM(
+CREATE TABLE IF NOT EXISTS Zawodnik (
+    ID_Zawodnika INTEGER(5) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+    Imie VARCHAR(30)  NOT NULL,
+    Nazwisko VARCHAR(40) NOT NULL,
+    DataUrodzenia DATE NOT NULL,
+    Plec ENUM(
       'M',  -- MEZCZYZNA
       'K'   -- KOBIETA
-      ) NOT NULL,
-  DataWaznosciLicencji DATE NOT NULL,
-  ID_Klubu INTEGER(5) DEFAULT NULL
+    ) NOT NULL,
+    ID_Klubu INTEGER(5) DEFAULT NULL
+) ENGINE = InnoDb;
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ ZAWODNIK_ZAWODY
+
+CREATE TABLE IF NOT EXISTS Zawodnik_Zawody (
+    ID_Zawodnika INTEGER(5) NOT NULL,
+    ID_Zawodow INTEGER(5) NOT NULL
+) ENGINE = InnoDb;
+
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- STRUKTURA TABELI REPREZENTUJACEJ KONKURENCJA
+
+CREATE TABLE IF NOT EXISTS Konkurencja (
+    ID_Konkurencji INTEGER(5) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    Styl ENUM(
+        'KRAUL',
+        'KLASYCZNY',
+        'GRZBIETOWY',
+        'MOTYLKOWY',
+        'DOWOLONY'
+    ) NOT NULL,
+    Dystans ENUM(
+        '50M',
+        '100M',
+        '200M',
+        '400M',
+        '10KM'
+    ) NOT NULL,
+    Kategoria ENUM(
+        'SZTAFETA',
+        'INDYWIDUALNE'
+    ) NOT NULL,
+    ID_Zawodow INTEGER(5) DEFAULT NULL,
+    ID_Sedziego INTEGER(5) DEFAULT NULL,
+    ID_Obiektu INTEGER(5) DEFAULT NULL
 ) ENGINE = InnoDB;
 
 -- -------------------------------------------------------------
 
 -- -------------------------------------------------------------
---                          KLUCZE OBCE
+--                       OGRANICZENIA
 -- -------------------------------------------------------------
 
 -- -------------------------------------------------------------
+-- TRENER
 
-ALTER TABLE pbd_plywanie.Trener
-ADD CONSTRAINT FOREIGN KEY (ID_Klubu) REFERENCES KlubPlywacki(ID_Klubu);
+ALTER TABLE Trener
+ADD CONSTRAINT FOREIGN KEY (ID_Klubu) REFERENCES KlubPlywacki(ID_Klubu),
+ADD CONSTRAINT FOREIGN KEY (ID_Licencji) REFERENCES Licencja(ID_Licencji);
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- KONKURENCJA
+
+ALTER TABLE Konkurencja
+ADD CONSTRAINT FOREIGN KEY (ID_Zawodow) REFERENCES Zawody(ID_Zawodow) -- ,
+-- ADD CONSTRAINT FOREIGN KEY (ID_Sedziego) REFERENCES Sedzia(ID_Sedziego),
+-- ADD CONSTRAINT FOREIGN KEY (ID_Obiektu) REFERENCES Obiekt(ID_Obiekt)
+;
+-- -------------------------------------------------------------
+
+-- -------------------------------------------------------------
+-- Zawodnik_Zawody
+
+ALTER TABLE Zawodnik_Zawody
+ADD CONSTRAINT FOREIGN KEY (ID_Zawodow) REFERENCES Zawody(ID_Zawodow),
+ADD CONSTRAINT FOREIGN KEY (ID_Zawodnika) REFERENCES Zawodnik(ID_Zawodnika);
+-- -------------------------------------------------------------
 
 -- -------------------------------------------------------------
